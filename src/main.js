c91fd091e5b13c8c3f173eb3368073e8bde45fdc
hzf <601061980@qq.com>
2023-08-09 18:47:40 +0800

    fix:0809

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import * as utils from './utils';
 export function createApp() {
   const app = createSSRApp(App);
   app.use(pinia);
+  app.config.globalProperties.$uni = uni;
   for (const k in utils) {
     app.config.globalProperties[`$${ k }`] = utils[k];
   }
